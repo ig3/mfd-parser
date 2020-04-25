b59757ad1a980cd977440f6ceaca5943fa183298
Ian Goodacre <Ian.Goodacre@entrain.nz>
2020-04-25 05:20:04 +0000

    eslint-entrain --fix

--- index.js
+++ index.js
@@ -1,62 +1,62 @@
-const T_EMPTY_STR = ''
-const T_EOL = /\r?\n/g
-const T_HEADER_KEYVAL = ':'
-const T_HEADER_META = ';'
-const T_HEADERS_BODY = /\r?\n\r?\n/g
+const T_EMPTY_STR = '';
+const T_EOL = /\r?\n/g;
+const T_HEADER_KEYVAL = ':';
+const T_HEADER_META = ';';
+const T_HEADERS_BODY = /\r?\n\r?\n/g;
 
-exports.parse = function parse(raw, boundary = null) {
-  if (null === boundary) {
-    boundary = raw.split(T_EOL)[0].trim()
+exports.parse = function parse (raw, boundary = null) {
+  if (boundary === null) {
+    boundary = raw.split(T_EOL)[0].trim();
   }
 
   return raw
-    .split(boundary)
-    .filter(empty)
-    .map(part)
-    .reduce(toData, {})
-}
+  .split(boundary)
+  .filter(empty)
+  .map(part)
+  .reduce(toData, {});
+};
 
-function toData(data, part) {
-  const { headers, body } = part
+function toData (data, part) {
+  const { headers, body } = part;
 
   const key = headers
-    .filter(header => header.name === 'Content-Disposition')
-    .reduce((key, header) => {
-      return header
-        .value
-        .split(T_HEADER_META)[1]
-        .trim()
-        .match(/name="(.+)"/)[1]
-    }, T_EMPTY_STR)
+  .filter(header => header.name === 'Content-Disposition')
+  .reduce((key, header) => {
+    return header
+    .value
+    .split(T_HEADER_META)[1]
+    .trim()
+    .match(/name="(.+)"/)[1];
+  }, T_EMPTY_STR);
 
   data[key] = {
     headers,
     value: typeof body === 'undefined' ? '' : body.trim()
-  }
+  };
 
-  return data
+  return data;
 }
 
-function part(rawPart) {
-  const [rawHeaders, rawBody] = rawPart.split(T_HEADERS_BODY)
-  const headers = rawHeaders.split(T_EOL).filter(empty).map(header)
-  const body = rawBody
+function part (rawPart) {
+  const [rawHeaders, rawBody] = rawPart.split(T_HEADERS_BODY);
+  const headers = rawHeaders.split(T_EOL).filter(empty).map(header);
+  const body = rawBody;
 
-  return { headers, body }
+  return { headers, body };
 }
 
-function header(rawHeader) {
-  const [rawName, rawValue] = rawHeader.split(T_HEADER_KEYVAL)
-  const name = rawName.trim()
-  const value = typeof rawValue === 'undefined' ? '' : rawValue.trim()
+function header (rawHeader) {
+  const [rawName, rawValue] = rawHeader.split(T_HEADER_KEYVAL);
+  const name = rawName.trim();
+  const value = typeof rawValue === 'undefined' ? '' : rawValue.trim();
 
-  return { name, value }
+  return { name, value };
 }
 
-function empty(rawPart) {
+function empty (rawPart) {
   if (rawPart.trim() === T_EMPTY_STR) {
-    return false
+    return false;
   }
 
-  return true
+  return true;
 }
