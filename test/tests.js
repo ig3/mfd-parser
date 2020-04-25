7f8c744c173b7889bccda0ae9f1a3862c6ebb27d
Ian Goodacre <Ian.Goodacre@entrain.nz>
2020-04-25 05:51:55 +0000

    Adapt tests to tap

---
+++ test/tests.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const tap = require('tap');
+const { parse } = require('../index');
+const fs = require('fs');
+const path = require('path');
+
+tap.test('parse', (t) => {
+  const data = parse(fs.readFileSync(path.resolve(__dirname, 'fixture-lf.txt'), 'utf8'));
+
+  t.test('should parse each part headers', (t) => {
+    t.strictSame(data.name.headers, [
+      { name: 'Content-Disposition', value: 'form-data; name="name"' }
+    ]);
+
+    t.strictSame(data.key.headers, [
+      { name: 'Content-Disposition', value: 'form-data; name="key"' }
+    ]);
+
+    t.end();
+  });
+
+  t.test('should parse a raw multipart/form-data text', (t) => {
+    t.equal(data.name.value, 'attachment.txt');
+    t.equal(data.key.value, 'data/3830719/attachment.txt');
+    t.end();
+  });
+
+  t.end();
+});
+
+tap.test('parse crlf', (t) => {
+  const data = parse(fs.readFileSync(path.resolve(__dirname, 'fixture-crlf.txt'), 'utf8'));
+
+  t.test('should parse each part headers', (t) => {
+    t.strictSame(data.name.headers, [
+      { name: 'Content-Disposition', value: 'form-data; name="name"' }
+    ]);
+
+    t.strictSame(data.key.headers, [
+      { name: 'Content-Disposition', value: 'form-data; name="key"' }
+    ]);
+
+    t.end();
+  });
+
+  t.test('should parse a raw multipart/form-data text', (t) => {
+    t.equal(data.name.value, 'attachment.txt');
+    t.equal(data.key.value, 'data/3830719/attachment.txt');
+    t.end();
+  });
+
+  t.end();
+});
